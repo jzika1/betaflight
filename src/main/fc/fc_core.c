66a6be78cfef14c0acf8b1a4f40901ffc8fc3988
Dominic Clifton <dominic.clifton@cleanflight.com>
2017-04-15 16:12:37 +0100

    CF/BF - Port VTX button from CF v1.x.
    Features:
    * Channel Cycle
    * Band Cycle
    * Power Cycle
    * Save Settings
    * Works with any VTX using VTX API.

--- src/main/fc/fc_core.c
+++ src/main/fc/fc_core.c
@@ -81,6 +81,13 @@
 
 // June 2013     V2.2-dev
 
+#ifdef VTX_RTC6705
+bool canUpdateVTX(void);
+#define VTX_IF_READY if (canUpdateVTX())
+#else
+#define VTX_IF_READY
+#endif
+
 enum {
     ALIGN_GYRO = 0,
     ALIGN_ACCEL = 1,
@@ -470,6 +477,10 @@ void processRx(timeUs_t currentTimeUs)
 
 #ifdef VTX_CONTROL
     vtxUpdateActivatedChannel();
+
+    VTX_IF_READY {
+        handleVTXControlButton();
+    }
 #endif
 }
 
